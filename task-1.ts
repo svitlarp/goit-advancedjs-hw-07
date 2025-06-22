daaf958f58be03b92085a42f2724e5a1fa10fe46
svitlarp <svitlanka.ripa@gmail.com>
2025-06-22 13:45:14 +0300

    hw-7

---
+++ task-1.ts
@@ -0,0 +1,21 @@
+// Завдання 1
+// Клас Student, який містить три властивості: name, age та grade. Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, напишіть скорочену ініціалізацію.
+
+// class Student {
+//   public name: string;
+//   public age: number;
+//   public grade: string;
+
+//   constructor(name: string, age: number, grade: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.grade = grade;
+//   }
+// }
+
+
+class Student {
+    constructor(name:string, age: number, grade: string) {}
+}
+
+export { };
---
+++ task-2.ts
@@ -0,0 +1,23 @@
+class Employee {
+    public name: string;
+    private department: string; 
+    protected salary: number;  
+  
+    constructor(name: string, department: string, salary: number) {
+      this.name = name;
+      this.department = department;
+      this.salary = salary;
+    }
+  
+    getEmployeeDetails() {
+      return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    }
+  }
+  
+  class Manager extends Employee {
+      constructor(name: string, department: string, salary: number) {
+        super(name, department, salary + 10000,); 
+      }
+}
+  
+
